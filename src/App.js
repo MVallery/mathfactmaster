9fd9937f83a4f4dc6047b05cc63e1c37f3301bb5
Melissa Vallery <vallery.melissa@gmail.com>
2020-11-10 20:47:30 -0600

    added input boxes, answer check, and message

--- src/App.js
+++ src/App.js
@@ -3,61 +3,47 @@ import React, { useState } from 'react';
 import Problems from './problems';
 
 function App() {
-  var count = 0;
   const [questionList, setQuestionList] = useState([]);
-  // const [divideButtonClicked, setDivideButtonClicked] = useState(false);
-
-  const [randomVar, setRandomVar] = useState(1); //[name of variable, function to change the value of the variable] = useState(___default value__)
-  var prob = ""
+  const [correctAnswer, setCorrectAnswer] = useState([])
+  const [inputAnswer, setInputAnswer] = useState("")
+  const [message, setMessage] = useState("")
   const handleMultiplyClick = () => {
-    console.log("Hello")
     var E = Math.floor(Math.random()*15+2);
     var G = Math.floor(Math.random()*15+2);
+    var answer = E*G
     var question = E+" * "+G+" = "+(E*G) + " ";
-    let tempList = questionList;
-    tempList.push(question);
-    setQuestionList([tempList]);
-    // setQuestionList([question]);
-    
-    // setQuestionList(concat[question])
-    // setQuestionList = question
-    // setQuestionList([]).concat([question])
-    // setQuestionList(questionList.concat[question]);
-    // setQuestionList(questionList.concat(question))
-    // questionList.push(question)
-  }
-    // for (i=1; i<10; i++) {}
     // let tempList = questionList;
     // tempList.push(question);
-    // setQuestionList(tempList);
-  
+    setQuestionList([question]);
+    setCorrectAnswer([answer])
+  }
+
   const handleDivideClick = () => {
     var G = Math.floor(Math.random()*9+2);
-    var E = Math.floor(Math.random()*9+2);
-    var T = E*G
-    var question = T+ " / "+G+" = "+ E + " ";
-    let tempList = questionList;
-    tempList.push(question);
-    setQuestionList([tempList]);
-  }
+    var answer = Math.floor(Math.random()*9+2);
+    var T = answer*G
+    var question = T+ " / "+G+" = "+ answer + " ";
+    // let tempList = questionList;
+    // tempList.push(question);
+    setQuestionList([question]);
+    setCorrectAnswer([answer])
 
-  const handleMultiplyDivideClick = () => {
-    setQuestionList(["no multdiv yet"])
   }
+  const handleInputAnswer = (e) => {
+    setInputAnswer(e.target.value)
+    console.log(inputAnswer)
+    console.log(message)
+  }
+const handleSubmitAnswer = (e) => {
+  e.preventDefault();
 
-  // const handleClick = (prob, q) => {
-  //   for  (i=1; i<=q; i++){
-  //     if (prob = "divide") {
-  //       handleDivideClick();
-  //     }
-  //     else if (prob = "multiply") {
-  //       handleMultiplyClick();
-  //     }
-  //     else if (prob = "multiplyDivide") {
-  //       handleMultiplyDivideClick();
-  //     }
-  //     else {console.log("no selection")};
-  // }}
+  if (inputAnswer == correctAnswer) {
+    setMessage("Great job!")
+  }
+  else {
+    setMessage("Uh oh try again! The right answer was " + correctAnswer)
+  }
+}
 
   return (
 <div>
@@ -65,8 +51,14 @@ function App() {
         <Problems questionList={questionList} />
         <button onClick= {() => {handleMultiplyClick();}}>Multiply Problem</button>
         <button onClick= {() => {handleDivideClick();}}>Divide Problem</button>
-
-        {/* <form><p><label><input type="checkbox" name="multiply" onChange={handleMultiplyClick}/>Multiply</label></p></form> */}
+        <form onSubmit = {handleSubmitAnswer}>
+        <textarea onChange= {handleInputAnswer}
+                  onSubmit = {handleSubmitAnswer}
+                  value = {inputAnswer}
+                  placeholder= "input answer"/>
+        <button type= "submit" form className="commentForm" onSubmit={handleSubmitAnswer}>Submit</button>
+        </form>
+        {message}
       </header>
     </div>
   )
